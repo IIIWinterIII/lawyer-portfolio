f613ddb279f4bf19f7cfd2143380c4d313e53793
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-24 18:44:13 +0300

    footer style sketches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import "../styles/components/Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTelegram,
+  faVk,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <footer>
+      <div className="left-logo-message">
+        <h1 className="logo">Logo</h1>
+        <div className="box-section">
+          <a href="-">
+            <FontAwesomeIcon icon={faWhatsapp} className="icon w" />
+          </a>
+          <a href="-">
+            <FontAwesomeIcon icon={faTelegram} className="icon t" />
+          </a>
+          <a href="-">
+            <FontAwesomeIcon icon={faVk} className="icon v" />
+          </a>
+        </div>
+      </div>
+
+      <div className="right-contact-footer">
+        <div className="nomber-end-works-day">
+          <a href="-" className="left-con">
+            <FontAwesomeIcon icon={faPhone} className="icon" />
+            <strong>+7 123 456-78-90</strong>
+          </a>
+          <div className="works-day">
+            <p>
+              <strong>Пн-Пт:</strong> 9:00 – 18:00
+            </p>
+            <p>
+              <strong>Сб-Вс:</strong> 9:00 – 18:00
+            </p>
+          </div>
+        </div>
+        <a href="-" className="rigth-con">
+          <FontAwesomeIcon icon={faEnvelope} className="icon" />
+          <strong>example@email.com</strong>
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/reutes.js
+++ src/reutes.js
@@ -1,40 +1,44 @@
-import Home from "./pages/Home.jsx"
-import Services from "./pages/Services.jsx"
-import About from "./pages/About.jsx"
-import FAQ from "./pages/FAQ.jsx"
-import Contact from "./pages/Contact.jsx"
+import Home from "./pages/Home.jsx";
+import Services from "./pages/Services.jsx";
+import About from "./pages/About.jsx";
+import FAQ from "./pages/FAQ.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const routes = [
-    {
-        path: "/",
-        component: Home,
-        name: "Главная",
-        icon: "",
-    },
-    {
-        path: "/services",
-        component: Services,
-        name: "Услуги",
-        icon: "",
-    },
-    {
-        path: "/about",
-        component: About,
-        name: "Обо мне",
-        icon: "",
-    },
-    {
-        path: "/FAQ",
-        component: FAQ,
-        name: "Часто задаваемые вопросы",
-        icon: "",
-    },
-    {
-        path: "/contact",
-        component: Contact,
-        name: "Контакты",
-        icon: "",
-    },
-]
+  {
+    path: "/",
+    component: Home,
+    name: "Главная",
+    icon: "",
+  },
+  {
+    path: "/services",
+    component: Services,
+    name: "Услуги",
+    icon: "",
+    subItems: [
+      { path: "/services/web-development", name: "Суды" },
+      { path: "/services/seo", name: "Состовление документов" },
+    ],
+  },
+  {
+    path: "/about",
+    component: About,
+    name: "Обо мне",
+    icon: "",
+  },
+  {
+    path: "/FAQ",
+    component: FAQ,
+    name: "Часто задаваемые вопросы",
+    icon: "",
+  },
+  {
+    path: "/contact",
+    component: Contact,
+    name: "Контакты",
+    icon: "",
+  },
+];
 
-export default routes
+export default routes;
